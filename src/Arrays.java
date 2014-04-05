f0ec3ab3b3df573f85994cfcb44ecf61d1908c72
Codenator <apoltarjonoks@gmail.com>
2014-04-05 12:36:50 +0200

    First commit

---
+++ src/Arrays.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Codenator on 04-Apr-14.
+ */
+public class Arrays {
+    public static void main(String[] args)
+    {
+        int аррай[];
+        аррай = new int[10];
+        аррай[2] = 45;
+        System.out.println(аррай[2]);
+
+        long[] myNumbers = {1,2,10,45,23};// создаёт и записсывает массив
+        System.out.println(myNumbers[4]); // пример выборки массива из 4 ячейки
+
+
+    }
+}
