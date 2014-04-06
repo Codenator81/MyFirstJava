ad0a4de128e078841a014dbfe2cfb1a01625fbc6
Codenator <apoltarjonoks@gmail.com>
2014-04-06 20:36:12 +0200

    OOP

---
+++ src/oop/Car.java
@@ -0,0 +1,15 @@
+package oop;
+
+/**
+ * Created by Codenator on 06-Apr-14.
+ */
+public class Car {
+    int idi;
+    String name;
+    public static void main(String args[]){
+        Car honda = new Car();
+        honda.name = "Honda";
+        System.out.println(honda.name);
+    }
+
+}
